bd2f19d5adcd99f3b5f539761866c2aa180733b2
RuslanOnline <scaineton@gmail.com>
2024-07-06 09:49:26 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,16 @@
+my_dict = {'Lara':2000 ,'Vova':1990 ,'Masha':2005}
+print(my_dict)
+print(my_dict.get('Vova'))
+print(my_dict.get('Nika'))
+my_dict.update({'Boris':1985 ,'Marika':1980})
+print(my_dict.pop('Lara'))
+print(my_dict)
+ # Множества
+my_set = {1 ,5 ,5 ,3 ,'Loot' ,5 ,'river'}
+print(my_set)
+my_set.update({'Mega' ,7})
+print(my_set)
+my_set.discard('Loot')
+print(my_set)
+
+
